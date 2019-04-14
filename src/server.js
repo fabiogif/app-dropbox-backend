a0e54b104782a19b23f4b1c2fa0e2ccc3ccdcec6
Fabio Santana <fabiosantanagif@gmail.com>
2019-04-13 21:12:02 -0300

    Estutura backend

---
+++ src/server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const cors = require('cors')
+const app = express()
+
+
+app.use(cors())
+
+io.on('connection', socket =>{
+    socket.on('connectRoom', box=>{
+        socket.join(box)
+    })
+})
+
+
+mongoose.connect(
+    'mongodb+srv://week6:week6@cluster0-dvqp5.mongodb.net/week6?retryWrites=true',
+    {
+    useNewUrlParser: true
+    })
+
+app.use((req, res, next) =>{
+    req.io = io
+
+    return next()
+})
+
+app.use(express.json())
+
+app.use(express.urlencoded({
+    extended: true
+}))// Criado para envio de arquivos
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')))
+// Caminho do arquivo
+
+app.use(require('./routes'))
+
+server.listen(3333)
